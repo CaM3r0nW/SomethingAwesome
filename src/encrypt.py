76add077cc337c8208a9b3eb8c3df373614f5f06
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-03-12 02:35:59 +1100

    Renamed encrypt to caesar_encrypt as there will be more encryption types. Added type error testing

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,6 +1,30 @@
+import string
 
-def encrypt(msg):
+"""
+Given a message it will shift each letter in the message by a given amount.
+The amount shifted is determined by the key
+Given a string and int
+Returns string
+"""
+def caesar_encrpt(msg, key):
+    if isinstance(msg, str) == False:
+        raise Exception("1st argument must be string")
+    if isinstance(key, int) == False:
+        raise Exception("2nd argument must be int")
+    for letter in msg:
+        letter_index = string.ascii_lowercase.index(letter)
+
+
+
+    return encrypted
+
+"""
+Decrypts the caesar encryption from above
+Given a string and int
+Returns string
+"""
+def caesar_decrypt(msg, key):
     return msg
 
-def decrypt(msg):
-    return msg
+if __name__ == '__main__':
+    caesar_encrpt("Larry",'Hat')
