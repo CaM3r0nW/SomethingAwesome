12a59cebd1c3b5ad07e2cf7334d8b2ac8d870de7
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-03-12 02:47:34 +1100

    caesar_encrypt untested but seemingly working

--- src/encrypt.py
+++ src/encrypt.py
@@ -7,14 +7,38 @@ Given a string and int
 Returns string
 """
 def caesar_encrpt(msg, key):
+    """
+    Ensures the given data is the correct type
+    """
     if isinstance(msg, str) == False:
         raise Exception("1st argument must be string")
     if isinstance(key, int) == False:
         raise Exception("2nd argument must be int")
+
+    """
+    ---------------------------------
+    """
+
+    encrypted = ''
+
+    #For each letter in the message shift by key
     for letter in msg:
-        letter_index = string.ascii_lowercase.index(letter)
+        #Determins which alphabet to use
+        if letter.isupper() == True:
+            alphabet = string.ascii_uppercase
+        else:
+            alphabet = string.ascii_lowercase
 
+        letter_index = alphabet.index(letter)
+        letter_index = letter_index+key
 
+        #If the key is over 25 ('z'), minus 26
+        while True:
+            if letter_index < 26:
+                break
+            letter_index -= 26
+
+        encrypted += alphabet[letter_index]
 
     return encrypted
 
@@ -27,4 +51,4 @@ def caesar_decrypt(msg, key):
     return msg
 
 if __name__ == '__main__':
-    caesar_encrpt("Larry",'Hat')
+    print(caesar_encrpt("Larry",27))
