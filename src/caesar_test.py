c82ab919985c3ac542210b04355334bdde0dceea
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-03-13 02:35:26 +1100

    Tests should gather all cases

--- src/caesar_test.py
+++ src/caesar_test.py
@@ -1,7 +1,35 @@
 import pytest
-import caesar as e
+from caesar import caesar_encrpt, caesar_decrypt
 
 
-#Tests what happens with invalid type input
+#Tests what happens with invalid input
 def test_invalid():
-    assert e.caesar_encrpt('cat',26) == 'cat'
+    with pytest.raises(TypeError):
+        assert caesar_encrpt(2,'hi')
+    with pytest.raises(TypeError):
+        assert caesar_encrpt('hi','hi')
+    with pytest.raises(TypeError):
+        assert caesar_encrpt(2,2)
+    with pytest.raises(TypeError):
+        assert caesar_encrpt('','')
+
+#Tests limits of shift
+def test_shifting():
+    assert caesar_encrpt('xyz', 1) == 'yza'
+    assert caesar_encrpt('abc', -1) == 'zab'
+    assert caesar_encrpt('xyz', 27) == 'yza'
+    assert caesar_encrpt('abc', -27) == 'zab'
+
+#Example of sentence
+def test_sentence():
+    assert caesar_encrpt('A space', 26) == 'A space'
+    assert caesar_encrpt('An_Underscore', 0) == 'An_Underscore'
+
+def test_decrypt():
+    cypher = caesar_encrpt('xyz', 1)
+    assert cypher == 'yza'
+    assert caesar_decrypt(cypher, 1) == 'xyz'
+
+    cypher = caesar_encrpt('A space', 26)
+    assert cypher == 'A space'
+    assert caesar_decrypt(cypher, 26)
