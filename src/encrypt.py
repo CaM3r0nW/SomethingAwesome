f0cba44e7c0a05c3c0e3dc8d3dad9be6e554b25e
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-03-12 21:47:37 +1100

    Seemingly working, requires testing

--- src/encrypt.py
+++ src/encrypt.py
@@ -33,10 +33,14 @@ def caesar_encrpt(msg, key):
         letter_index = letter_index+key
 
         #If the key is over 25 ('z'), minus 26
+        #If the key is under 0 ('a'), plus 26
         while True:
-            if letter_index < 26:
+            if letter_index > 26:
+                letter_index -= 26
+            elif letter_index < 0:
+                letter_index += 26
+            else:
                 break
-            letter_index -= 26
 
         encrypted += alphabet[letter_index]
 
@@ -48,7 +52,46 @@ Given a string and int
 Returns string
 """
 def caesar_decrypt(msg, key):
-    return msg
+        """
+        Ensures the given data is the correct type
+        """
+        if isinstance(msg, str) == False:
+            raise Exception("1st argument must be string")
+        if isinstance(key, int) == False:
+            raise Exception("2nd argument must be int")
+
+        """
+        ---------------------------------
+        """
+
+        decrypted = ''
+
+        #For each letter in the message shift by key
+        for letter in msg:
+            #Determins which alphabet to use
+            if letter.isupper() == True:
+                alphabet = string.ascii_uppercase
+            else:
+                alphabet = string.ascii_lowercase
+
+            letter_index = alphabet.index(letter)
+            letter_index = letter_index-key
+
+            #If the key is over 25 ('z'), minus 26
+            #If the key is under 0 ('a'), plus 26
+            while True:
+                if letter_index > 26:
+                    letter_index -= 26
+                elif letter_index < 0:
+                    letter_index += 26
+                else:
+                    break
+
+            decrypted += alphabet[letter_index]
+
+        return decrypted
+
+
 
 if __name__ == '__main__':
-    print(caesar_encrpt("Larry",27))
+    print(caesar_decrypt(caesar_encrpt("La rry",5),5))
