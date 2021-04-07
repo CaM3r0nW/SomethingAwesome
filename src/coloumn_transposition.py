d4f8ea55531156830b7af40d7351945b9820cb32
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-04-07 16:21:28 +1000

    Made stubs for transposition cypher

---
+++ src/coloumn_transposition.py
@@ -0,0 +1,8 @@
+import string
+from english_words import english_words_alpha_set
+
+def columnar_transposition_encrypt(to_encrypt, key):
+    return to_encrypt
+
+def columnar_transposition_decrypt(encrypted, key):
+    return encrypted
