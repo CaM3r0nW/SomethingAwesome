cf2454c038babad276ae8c007e5da716bb57f5fa
CaM3r0nW <cameron.white@student.unsw.edu.au>
2021-04-10 16:46:05 +1000

    Commented basic structure for transposition

--- src/coloumn_transposition.py
+++ src/coloumn_transposition.py
@@ -2,6 +2,10 @@ import string
 from english_words import english_words_alpha_set
 
 def columnar_transposition_encrypt(to_encrypt, key):
+    #Find the length of the key
+    #Break to_encrypt into a number of lists equal in length to key
+    #Associate each coloumn with a letter
+    #Sort letters alphabetically, the return each coloumn together as one string
     return to_encrypt
 
 def columnar_transposition_decrypt(encrypted, key):
